31f91604a6cf476e562a991739b8fe0156089c7f
mwuf15 <mwuf15@gmail.com>
2021-10-16 22:30:46 -0700

    added graph to data set and styling changes

--- src/TaskEntry.js
+++ src/TaskEntry.js
@@ -10,9 +10,9 @@ function TaskEntry ({task}) {
 
     return (
         <div className="TaskEntry">
-            <label for="task"> Enter Task: </label>
-            <input name='taskEntry' type='text' onChange = {e => setTaskInput(e.target.value)} id="taskEntry"/> <br/>
-            <label for="time"> Enter Hours: </label>
+            <label id="enterTask" for="task"> Enter Task: </label>
+            <input name="taskEntry" type="text" onChange = {e => setTaskInput(e.target.value)} id="taskEntry"/>
+            <label id="enterTime" for="time"> Enter Hours: </label>
             <input name="hourEntry" type="text" id="hourEntry" onChange = {e => {
                 if(isNaN(Number(e.target.value))) {
                     setDisabled(true);
@@ -22,8 +22,8 @@ function TaskEntry ({task}) {
                 }
             }} 
             /> 
-            <br/>
-            <button disabled = {disabled} onClick={() => {
+            
+            <button className='submitButton' disabled = {disabled} onClick={() => {
                 task(taskInput,hourInput);
                 setDisabled(true);
             }}>Submit</button>
